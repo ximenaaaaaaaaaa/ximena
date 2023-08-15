ccb2b2d79881688f89e52128633d345eafa91e18
ximenaaaaaaaaaa <142330713+ximenaaaaaaaaaa@users.noreply.github.com>
2023-08-15 10:03:36 -0600

    Add files via upload

---
+++ Generaciones de la Computadora.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main (int argc,char **argv)
+{	
+setlocale(LC_ALL, "");
+
+		printf("										GENERACIONES DE LA COMPUTADORA\n");
+		printf("\n");
+		printf("	Primera Generacion (1940-1956):\n");
+printf("Durante esta epoca, las computadoras utilizaban tubos de vacio como componentes principales. Eran grandes, costosas y requerian mucho espacio y energia\n");
+		printf("\n");
+		printf("	Segunda Generacion (1956-1963)\n");
+printf("Los transistores reemplazaron los tubos de vacio, lo que permitio computadoras mas pequeñas, rápidas y eficientes. Surgieron lenguajes de programación de alto nivel y se comenzaron a utilizar sistemas de almacenamiento magnético, como cintas y discos.\n");
+		printf("\n");
+		printf("	Tercera Generacion (1964-1971)\n");
+printf("Se introdujeron los circuitos integrados (chips) que contenían múltiples transistores en un solo componente. Esto llevo a una mayor miniaturizacion y a un mejor rendimiento\n");
+		printf("\n");
+		printf("	Cuarta Generacion (1971-1980)\n");
+printf("Se desarrollaron los microprocesadores, que incluían la CPU en un solo chip. Esto permitió la creación de computadoras personales\n");
+		printf("\n");
+		printf("	Quinta Generacion (a partir de 1980)\n");
+printf("Se caracteriza por el desarrollo de la tecnologia de circuitos integrados de muy alta escala (VLSI) y microprocesadores más potentes.\n");
+		printf("\n");
+		printf("\n");
+		printf("Caracteristicas\n");
+		printf("\n");
+		printf("	Primera Generacion\n");
+		printf("\n");
+printf("Tubos de vacio: Estos tubos eran dispositivos grandes y frágiles que amplificaban y conmutaban señales eléctricas\n");
+printf("Tamaño y consumo de energía: Las computadoras de primera generación eran enormes y ocupaban grandes salas.\n");
+printf("Programación a nivel de lenguaje de máquina: La programación de las computadoras de primera generación se realizaba a nivel de lenguaje de máquina, lo que significaba que los programadores tenían que codificar instrucciones utilizando patrones de bits específicos para realizar tareas.\n");
+		printf("\n");
+		printf("	Segunda Generacion\n");
+		printf("\n");
+printf("Transistores: son dispositivos electrónicos más pequeños, más eficientes y más confiables que los tubos de vacio, lo que permitio un aumento significativo en la velocidad de procesamiento y una reducción considerable en el tamaño y el consumo de energía de las computadoras\n");
+printf("Tamaño más pequeño\n");
+printf("Uso de lenguajes de programación de alto nivel: Durante la segunda generación, se comenzaron a desarrollar y utilizar lenguajes de programacion de alto nivel, como el COBOL y el FORTRAN\n");
+		return 0;
+		return EXIT_SUCCESS;
+}
+
